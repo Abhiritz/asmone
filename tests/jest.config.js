5157b2706179fab12401aaa8364534deb908beb4
Ritwik Abhijeet <ritwikabhijeet@gmail.com>
2024-10-16 17:41:49 +0530

    config errors fixed

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-  };
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+};
