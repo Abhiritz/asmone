7f33357b392c83c8c2595781cc842ca4b0a581d5
Ritwik Abhijeet <ritwikabhijeet@gmail.com>
2024-10-17 13:03:34 +0530

    assessment six, ignore numbers exceeding 1000

--- src/adder/adder.test.ts
+++ src/adder/adder.test.ts
@@ -3,54 +3,15 @@ import type * as tstfunc from './index';
 const {add} = jest.requireActual<typeof tstfunc>("./index.ts")
 
 const successCases = [
+ 
     {
         id: 0,
-        input: {i:'1,2'},
-        output: 3
-    },
-    {
-        id: 1,
-        input: {i:'1,2,3'},
-        output: 6
-    },
-    {
-        id: 2,
-        input: {i:'1'},
-        output: 1
-    },
-    {
-        id: 3,
-        input: {i:''},
-        output: 0
-    },
-    {
-        id: 3,
-        input: {i:'1,2,3,6666,8,9,0,1,5,8,9,3,555,09,79,80'},
-        output: 7438
-    },
-    {
-        id: 4,
-        input: {i:'2,4\n34'},
-        output: 40
-    },
-    {
-        id: 5,
-        input: {i:' 1;2/3*4 5_6,7\n8"9\'10'},
+        input: {i:' 1;2/3*4, _5,6,7\n8"9\'10_1005_2024/9999999'},
         output: 55
-    },
-    {
-        id: 6,
-        input: {i:' 1;2/3*4, -5,6_7\n8"9\'10'},
-        output: Error
-    },
-    {
-        id: 7,
-        input: {i:' 1;2/3*4, -5,-6,7\n8"9\'10_-7/-8'},
-        output: Error
     },
 ];
 
-describe('asm five, negative numbers throw exception as mentioned',() => {
+describe('asm six, ignore numbers exceeding 1000',() => {
     it.each(successCases)("test case $id input: $input output: $output",({input,output})=>{
         const {i} = input;
         expect(add(i)).toBe(output)
--- src/adder/index.ts
+++ src/adder/index.ts
@@ -3,11 +3,12 @@ export function add (numbers:string): number {
     let num  = numbers.split(/[_,\n ;/'"*@]/).map(Number).reduce((a,b)=>{
         if  (b < 0) {
             negatives !== '' ? negatives += ', ' + b : negatives += b ;
-        }
+        }else if(b > 1000) return  a;
         return  a+b
     }, 0)
     if(negatives.length > 0){
         throw new Error('negative numbers are not allowed '+negatives)
     }
+
     return num;
 }
