00686a8df0861bf1df94a121d85fcc51a3fa7ff3
Ritwik Abhijeet <ritwikabhijeet@gmail.com>
2024-10-17 13:08:27 +0530

    assessment seven, can take delimiters of any length between numbers

--- src/adder/adder.test.ts
+++ src/adder/adder.test.ts
@@ -9,9 +9,14 @@ const successCases = [
         input: {i:' 1;2/3*4, _5,6,7\n8"9\'10_1005_2024/9999999'},
         output: 55
     },
+    {
+        id: 1,
+        input: {i:' 1;_\\/    **2/3*4, _5,6,7\n8"9\'10_1005_2024/9999999'},
+        output: 55
+    },
 ];
 
-describe('asm six, ignore numbers exceeding 1000',() => {
+describe('asm seven, can take delimiters of any length between numbers',() => {
     it.each(successCases)("test case $id input: $input output: $output",({input,output})=>{
         const {i} = input;
         expect(add(i)).toBe(output)
--- src/adder/index.ts
+++ src/adder/index.ts
@@ -1,6 +1,6 @@
 export function add (numbers:string): number {
     let negatives = '';
-    let num  = numbers.split(/[_,\n ;/'"*@]/).map(Number).reduce((a,b)=>{
+    let num  = numbers.split(/[_,\n ;\\/'"*@]/).map(Number).reduce((a,b)=>{
         if  (b < 0) {
             negatives !== '' ? negatives += ', ' + b : negatives += b ;
         }else if(b > 1000) return  a;
